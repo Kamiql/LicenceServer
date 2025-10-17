4835f7e346059f3dcb763e8f4c85c00259ae8f0c
Kilian Radtke <kilian.radtke@adesso.de>
2025-10-17 15:16:25 +0200

    initial() Work in progress

---
+++ src/main/kotlin/dev/kamiql/Main.kt
@@ -0,0 +1,10 @@
+package dev.kamiql
+
+import io.ktor.server.engine.embeddedServer
+import io.ktor.server.netty.Netty
+
+fun main() {
+    embeddedServer(Netty, port = 8080) {
+
+    }.start(wait = true)
+}
